cdddf957bae339187f5da752fbab5ac37f58dbc0
longnh176040 <45277447+longnh176040@users.noreply.github.com>
2022-05-05 23:09:21 +0700

    no mes

--- MFGACOB/src/core/Population.java
+++ MFGACOB/src/core/Population.java
@@ -4,12 +4,24 @@ import java.util.ArrayList;
 
 public class Population {
     ArrayList<Individual> pop;
+    Task[] tasks;
     int chromosomeLength = -1;
+    int taskNum;
 
     public Population(Task[] tasks) {
+        this.taskNum = tasks.length;
+        this.tasks = tasks;
         for(Task t: tasks){
             this.chromosomeLength = Math.max(this.chromosomeLength, t.domainNumber);
         }
-        pop = new ArrayList<Individual>();
+        this.pop = new ArrayList<Individual>();
+    }
+
+    public void InitPopulation() {
+        for (int i = 0; i < Settings.MFEA_POPULATION_SIZE; i++) {
+            Individual individual = new Individual(taskNum, chromosomeLength);
+            individual.RandomInit();
+            individual.EvaluateFitness(tasks);
+        }
     }
 }
